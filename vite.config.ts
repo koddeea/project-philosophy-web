c00829f7ed100c406e78b89de4bc8588b35687ce
koddeea <koddea@protonmail.com>
2024-05-16 03:47:23 +0200

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
 	plugins: [react()],
-	base: "/project-philosophy-web/",
+	base: "https://koddeea.github.io/project-philosophy-web/",
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
